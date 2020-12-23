6f9d4d9da5bb06b8941e1b88f794cf601ab403c4
Jordi Forés <xThalach@gmail.com>
2020-12-23 20:36:28 +0100

    Testinng
    now it works?

--- Mimikatz.java
+++ Mimikatz.java
@@ -1,1 +1,1 @@
-Hii this a test just to know how its works . I dont know why this is not working.
+Hii this a test just to know how its works . I dont know why this is not working. Let me see if this works.
